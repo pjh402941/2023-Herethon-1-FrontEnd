066637bf4e75d9d37cbcec928596b56a86bcbe3a
pjh <pjh402941@gmail.com>
2023-07-14 09:39:40 +0900

    파일 업로드 완성

--- js/password-check.js
+++ js/password-check.js
@@ -1,7 +1,7 @@
 function isSame() {
   var pw = document.getElementById("upw1").value;
   var confirmPW = document.getElementById("upw2").value;
-  if (pw.length < 6 || pw.length > 16) {
+  if (pw.length < 8 || pw.length > 16) {
     window.alert("비밀번호는 6글자 이상, 16글자 이하만 이용 가능합니다.");
     document.getElementById("upw1").value = document.getElementById(
       "upw2"
